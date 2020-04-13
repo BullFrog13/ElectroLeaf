78b7abe77b67ed28987d3a0b2dcabd44c0be0b00
Vadym Harkusha <vadgig@gmail.com>
2020-04-13 19:57:13 -0400

    Added IP field in DeviceDector and styled page

--- src/components/DeviceDetector/index.js
+++ src/components/DeviceDetector/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
 import DetailsOutlinedIcon from '@material-ui/icons/DetailsOutlined';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import DetailsSharpIcon from '@material-ui/icons/DetailsSharp';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { NanoleafClient, ServiceDiscovery } from 'nanoleaf-client';
+import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: '100vh',
+    display: 'flex',
+  },
+  grid: {
+    marginTop: -theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -25,87 +24,44 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
   submit: {
     margin: theme.spacing(3, 0, 2),
     background: theme.palette.button.redGradient,
   },
+  grayText: {
+    color: theme.palette.primary.light,
+  },
   whiteText: {
     color: theme.palette.primary.contrastText,
   },
-  list: {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    marginTop: theme.spacing(3),
+  ipTextField: {
+    marginTop: theme.spacing(2),
+    width: '100%',
   },
 }));
 
 export default function SignIn() {
   const classes = useStyles();
 
-  // const sd = new ServiceDiscovery();
-  // sd.discoverNanoleaf().then(res => {
-  //   console.log(res);
-  // });
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs" className={classes.container}>
       <CssBaseline />
-      <div className={classes.paper}>
+      <Grid container justify="center" className={classes.grid}>
         <Avatar className={classes.avatar}>
           <DetailsOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5" className={classes.whiteText}>
           Locate Device
         </Typography>
-        <List className={classes.list}>
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <DetailsSharpIcon />
-            </ListItemAvatar>
-            <ListItemText
-              primary="Brunch this weekend?"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="textPrimary"
-                  >
-                    Ali Connors
-                  </Typography>
-                  {" — I'll be in your neighborhood doing errands this…"}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-          <Divider variant="inset" component="li" />
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <DetailsSharpIcon />
-            </ListItemAvatar>
-            <ListItemText
-              primary="Summer BBQ"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="textPrimary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                  {" — Wish I could come, but I'm out of town this…"}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-        </List>
-
+        <TextField
+          id="filled-basic"
+          label="Enter the Device IP"
+          variant="filled"
+          color="secondary"
+          className={classes.ipTextField}
+          InputLabelProps={{ className: classes.grayText }}
+          InputProps={{ className: classes.whiteText }}
+        />
         <Button
           type="submit"
           fullWidth
@@ -115,7 +71,7 @@ export default function SignIn() {
         >
           Continue
         </Button>
-      </div>
+      </Grid>
     </Container>
   );
 }
