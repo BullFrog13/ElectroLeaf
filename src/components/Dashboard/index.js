b6595a6a1faa28679f970ed2e9c035cf83c9302c
BullFrog13 <13heavyweather@gmail.com>
2020-04-22 02:08:57 -0400

    Added dashboard cards.

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -9,7 +9,6 @@ import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -18,7 +17,15 @@ import { useHistory } from 'react-router-dom';
 import AppsIcon from '@material-ui/icons/Apps';
 import Switch from '@material-ui/core/Switch';
 import Slider from '@material-ui/core/Slider';
-import ColorPicker from 'material-ui-color-picker'
+import ColorPicker from 'material-ui-color-picker';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import NanoleafImage from '../../assets/images/img.jpg';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { red } from '@material-ui/core/colors';
 
 const drawerWidth = 240;
 
@@ -43,7 +50,13 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  media: {
+    height: 180,
+  },
   toolbar: theme.mixins.toolbar,
+  avatar: {
+    backgroundColor: red[500],
+  },
 }));
 
 export default function Dashboard(props) {
@@ -52,14 +65,12 @@ export default function Dashboard(props) {
 
   if (!history.location.state) history.push('');
 
-  console.log(history.location.state.host);
-
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   const [state, setState] = React.useState({
     checkedA: true,
     checkedB: true,
-    value: 30
+    value: 30,
   });
 
   const [value, setValue] = React.useState(30);
@@ -110,22 +121,47 @@ export default function Dashboard(props) {
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            <Grid style={{ backgroundColor: 'green' }} item xs={12} md={8} lg={9}>
-              <div>
+            <Grid style={{ backgroundColor: 'green' }} item xs={12} md={6} lg={6}>
+              {/* <div>
                 <Switch
                   checked={state.checkedA}
                   onChange={handleChange}
                   name="checkedA"
                   inputProps={{ 'aria-label': 'secondary checkbox' }}
                 />
-              </div>
+              </div> */}
+
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={NanoleafImage}
+                    title="Contemplative Reptile"
+                  />
+                </CardActionArea>
+              </Card>
             </Grid>
-            <Grid style={{ backgroundColor: 'red' }} item xs={12} md={4} lg={3}>
-              <ColorPicker
+
+            <Grid style={{ backgroundColor: 'red' }} item xs={12} md={6} lg={6}>
+              {/* <ColorPicker
                 name='color'
                 defaultValue='#000'
                 // value={this.state.color} - for controlled component
-                onChange={color => console.log(color)} />
+                onChange={color => console.log(color)} /> */}
+
+              <Card className>
+                <CardMedia
+                  className={classes.media}
+                  image="/static/images/cards/paella.jpg"
+                  title="Paella dish"
+                />
+                <CardContent>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    This impressive paella is a perfect party dish and a fun meal to cook together with your
+                    guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        </Typography>
+                </CardContent>
+              </Card>
             </Grid>
             <Grid style={{ backgroundColor: 'blue' }} item xs={12}>
               <Slider value={value} onChange={handleSliderChange} aria-labelledby="continuous-slider" />
